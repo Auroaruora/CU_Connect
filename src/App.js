00c5f73769fc968ff7b13469b2db56951a442b54
Auroaruora <zw2985@columbia.edu>
2024-11-19 16:39:11 -0500

    Implement the liked Club Page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
+import Icons8TrashCan1001Image from './assets/icons8-trash-can-100.png';
+import BPLSImage from './assets/4.png';
+import ODTImage from './assets/1.png';
+import SWEImage from './assets/2.png';
+import CSImage from './assets/3.png';
+
+function LikedScreen() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <div className="liked-screen">
+          <div className="content-container">
+              <div className="liked-clubs">Liked Clubs</div>
+              <div className="groups-container">
+                  <div className="group">
+                      <div className="rectangle"></div>
+                      <img className="trash-icon" src={Icons8TrashCan1001Image} alt="Trash Can Icon" />
+                      <div className="club-name">Black Pre-Law Society</div>
+                      <img className="screenshot" src={BPLSImage} alt="Screenshot 1" />
+                  </div>
+                  <div className="group">
+                      <div className="rectangle"></div>
+                      <img className="trash-icon" src={Icons8TrashCan1001Image} alt="Trash Can Icon" />
+                      <div className="club-name">Onyx Dance Troupe</div>
+                      <img className="screenshot" src={ODTImage} alt="Screenshot 2" />
+                  </div>
+                  <div className="group">
+                      <div className="rectangle"></div>
+                      <img className="trash-icon" src={Icons8TrashCan1001Image} alt="Trash Can Icon" />
+                      <div className="club-name"style={{ fontSize: '14px' }}>Society of Women Engineers</div>
+                      <img className="screenshot" src={SWEImage} alt="Screenshot 3" />
+                  </div>
+                  <div className="group">
+                      <div className="rectangle"></div>
+                      <img className="trash-icon" src={Icons8TrashCan1001Image} alt="Trash Can Icon" />
+                      <div className="club-name">Columbia Spectator</div>
+                      <img className="screenshot" src={CSImage} alt="Screenshot 4" />
+                  </div>
+              </div>
+          </div>
+      </div>
     </div>
   );
 }
 
-export default App;
+
+
+export default LikedScreen;
